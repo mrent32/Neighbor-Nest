2dafd2bdd50be388ea41a3924e19a1c635bdff7c
Martin Renteria <mrrenteria0159@gmail.com>
2024-03-13 21:37:11 -0600

    fixing package json

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Neighbor-Nest', {
+mongoose.connect('mongodb+srv://mrrenteria0159:eqy1jDEqPeFtsfw2@neighbor-nest.hryr8li.mongodb.net/?retryWrites=true&w=majority&appName=Neighbor-Nest', {
     serverSelectionTimeoutMS: 5000, // Increase timeout to 5 seconds
 });
 
 module.exports = mongoose.connection;
+
+
+// mongodb+srv://mrrenteria0159:eqy1jDEqPeFtsfw2@neighbor-nest.hryr8li.mongodb.net/?retryWrites=true&w=majority&appName=Neighbor-Nest
+
+// mongodb+srv://mrrenteria0159:eqy1jDEqPeFtsfw2@neighbor-nest.hryr8li.mongodb.net/?retryWrites=true&w=majority&appName=Neighbor-Nest
